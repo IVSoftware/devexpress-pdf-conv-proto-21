3a42975449044bd15b4485d1c628096d187ab9b9
Tom Gregor <tgregor@ivsoftware.com>
2021-09-22 12:36:17 -0600

    Add project files.

---
+++ devexpress-pdf/devexpress-pdf/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace devexpress_pdf
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+    class Binding : INotifyPropertyChanged
+    {
+        public Binding()
+        {          
+            DocxToPdfCommand = new Command(OnDocxToPdf);
+        }
+        public ICommand DocxToPdfCommand { get; }
+        private void OnDocxToPdf(object o)
+        {
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
